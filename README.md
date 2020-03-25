2ea845f9dcd997991911e6872d41c5635f0b7c13
SCOTT <scott.kutler@gmail.com>
2020-03-25 16:35:07 -0400

    event listeners complete. buttons setup.

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ https://wallpaperaccess.com/amazing-abstract
 
 Speech Synthesis Code from:
 https://stackoverflow.com/questions/15653145/using-google-text-to-speech-in-javascript
+
+Grid DIV itterator code based on help at:
+https://codereview.stackexchange.com/questions/157895/create-multiple-divs-inside-a-containing-div
+document.write('<div class="gridbox" value='+i+'>'+ i.toString() + '</div>');
+
+for(let i=0;i<4;i++)
+{
+[gridbox[i],gridbox[i+8],gridbox[i+16],gridbox[i+24]].foreach(function (element)
+{
+element.addEventListener("click", displayTop);
+});
+}
--- main.js
+++ main.js
@@ -5,26 +5,31 @@ let bottomColor = (activatedDiv.style.borderBottomColor = "gold");
 let leftColor = (activatedDiv.style.borderLeftColor = "green");
 let msg = "";
 let restartButton = document.querySelector(".restart");
+let overlay = document.querySelector("#overlay");
+let gridbox = document.querySelectorAll(".gridbox");
+let hardMode = document.querySelector(".hardMode");
+let computerValues = [];
+let playerValues = [];
 
-// Testing ALL Button Activation
-function activateAll() {
-  topColor = activatedDiv.style.borderTopColor = "pink";
-  rightColor = activatedDiv.style.borderRightColor = "cornflowerblue";
-  bottomColor = activatedDiv.style.borderBottomColor = "lemonchiffon";
-  leftColor = activatedDiv.style.borderLeftColor = "lawngreen";
-  msg = new SpeechSynthesisUtterance("Welcome to Simon. Please pay attention.");
-  //Best Voices: Daniel, Karen, Tessa (british)
-  //https://codepen.io/matt-west/full/wGzuJ
-  window.speechSynthesis.speak(msg);
+//msg = new SpeechSynthesisUtterance("Welcome to Simon.");
+//window.speechSynthesis.speak(msg);
+//Best Voices: Daniel, Karen, Tessa (british)
+//https://codepen.io/matt-west/full/wGzuJ
+
+function unclick() {
+  topColor = activatedDiv.style.borderTopColor = "red";
+  rightColor = activatedDiv.style.borderRightColor = "blue";
+  bottomColor = activatedDiv.style.borderBottomColor = "gold";
+  leftColor = activatedDiv.style.borderLeftColor = "green";
 }
 
 // Reset to Starter Colors
 function deactivateAll() {
   topColor = activatedDiv.style.borderTopColor = "red";
   rightColor = activatedDiv.style.borderRightColor = "blue";
-  bottomColor = activatedDiv.style.borderBottomColor = "yellow";
+  bottomColor = activatedDiv.style.borderBottomColor = "gold";
   leftColor = activatedDiv.style.borderLeftColor = "green";
-  msg = new SpeechSynthesisUtterance("Welcome to Simon. Please pay attention.");
+  msg = new SpeechSynthesisUtterance("game reset");
   //Best Voices: Daniel, Karen, Tessa (british)
   //https://codepen.io/matt-west/full/wGzuJ
   window.speechSynthesis.speak(msg);
@@ -32,26 +37,119 @@ function deactivateAll() {
 
 // Individual Lightup Functions
 function displayTop() {
+  unclick();
   topColor = activatedDiv.style.borderTopColor = "hotpink";
+  msg = new SpeechSynthesisUtterance("red");
+  window.speechSynthesis.speak(msg);
+  setInterval(unclick, 600);
 }
-
 function displayRight() {
-  rightColor = activatedDiv.style.borderRightColor = "cornflowerblue";
+  unclick();
+  rightColor = activatedDiv.style.borderRightColor = "lightblue";
+  msg = new SpeechSynthesisUtterance("blue");
+  window.speechSynthesis.speak(msg);
+  setInterval(unclick, 600);
 }
-
 function displayBottom() {
-  bottomColor = activatedDiv.style.borderBottomColor = "lemonchiffon";
+  unclick();
+  bottomColor = activatedDiv.style.borderBottomColor = "yellow";
+  msg = new SpeechSynthesisUtterance("yellow");
+  window.speechSynthesis.speak(msg);
+  setInterval(unclick, 600);
+}
+function displayLeft() {
+  unclick();
+  leftColor = activatedDiv.style.borderLeftColor = "lightgreen";
+  msg = new SpeechSynthesisUtterance("green");
+  window.speechSynthesis.speak(msg);
+  setInterval(unclick, 600);
 }
 
-function displayLeft() {
-  leftColor = activatedDiv.style.borderLeftColor = "green";
+function hardModeActivate() {
+  msg = new SpeechSynthesisUtterance("hard mode");
+  window.speechSynthesis.speak(msg);
+  setInterval(unclick, 600);
 }
+
+[
+  gridbox[1],
+  gridbox[2],
+  gridbox[3],
+  gridbox[9],
+  gridbox[10],
+  gridbox[11],
+  gridbox[16],
+  gridbox[17],
+  gridbox[18],
+  gridbox[19],
+  gridbox[24],
+  gridbox[25],
+  gridbox[26]
+].forEach(function(element) {
+  element.addEventListener("click", displayTop);
+});
+
+[
+  gridbox[4],
+  gridbox[5],
+  gridbox[12],
+  gridbox[13],
+  gridbox[14],
+  gridbox[20],
+  gridbox[21],
+  gridbox[22],
+  gridbox[23],
+  gridbox[29],
+  gridbox[30],
+  gridbox[31]
+].forEach(function(element) {
+  element.addEventListener("click", displayRight);
+});
+
+[
+  gridbox[37],
+  gridbox[38],
+  gridbox[39],
+  gridbox[44],
+  gridbox[45],
+  gridbox[46],
+  gridbox[47],
+  gridbox[52],
+  gridbox[53],
+  gridbox[54],
+  gridbox[60],
+  gridbox[61]
+].forEach(function(element) {
+  element.addEventListener("click", displayBottom);
+});
+
+[
+  gridbox[32],
+  gridbox[33],
+  gridbox[34],
+  gridbox[40],
+  gridbox[41],
+  gridbox[42],
+  gridbox[43],
+  gridbox[49],
+  gridbox[50],
+  gridbox[51],
+  gridbox[58],
+  gridbox[59]
+].forEach(function(element) {
+  element.addEventListener("click", displayLeft);
+});
 
 restartButton.addEventListener("click", deactivateAll);
 
-// Event Listeners for Clicks on SIMON BOARD:
-// need to fix to be for border of DIV
-// activatedTop.addEventListener("mouseover", displayTop);
-// activatedRight.addEventListener("mouseover", displayRight);
-// activatedBottom.addEventListener("mouseover", displayBottom);
-// activatedLeft.addEventListener("mouseover", displayLeft);
+[gridbox[56], hardMode].forEach(function(element) {
+  element.addEventListener("click", hardModeActivate);
+});
+
+/* Could have used this code to itterate, but it wouldnt be as exact:
+for (let i = 0; i < 4; i++) {
+  [gridbox[i], gridbox[i + 8], gridbox[i + 16], gridbox[i + 24]].foreach(
+    function(element) {
+      element.addEventListener("click", displayTop);
+    });}
+*/
