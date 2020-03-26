fd4b2b8cc263c7a7943ca537ac33b582df5125f3
SCOTT <scott.kutler@gmail.com>
2020-03-26 15:49:46 -0400

    added start button, scoreboard, calculating random color into array

--- main.js
+++ main.js
@@ -1,17 +1,27 @@
 let activatedDiv = document.querySelector(".inactive");
 let topColor = (activatedDiv.style.borderTopColor = "red");
+topColor.value = 0;
 let rightColor = (activatedDiv.style.borderRightColor = "blue");
+rightColor.value = 1;
 let bottomColor = (activatedDiv.style.borderBottomColor = "gold");
+bottomColor.value = 2;
 let leftColor = (activatedDiv.style.borderLeftColor = "green");
+leftColor.value = 3;
 let msg = "";
 let restartButton = document.querySelector(".restart");
 let overlay = document.querySelector("#overlay");
 let gridbox = document.querySelectorAll(".gridbox");
 let hardMode = document.querySelector(".hardMode");
-let computerValues = [];
-let playerValues = [];
+let computerValues = [0];
+let playerValues = [""];
 let hardModeStatus = false;
 let muted = 0;
+let score = 0;
+let scoreboard = document.querySelector(".scoreboard");
+let randomNumber = null;
+let round = 0;
+let startButton = document.querySelector(".starter");
+let gameinProgress = false;
 
 //msg = new SpeechSynthesisUtterance("Welcome to Simon.");
 //window.speechSynthesis.speak(msg);
@@ -26,7 +36,8 @@ function unclick() {
 }
 
 // Reset to Starter Colors
-function deactivateAll() {
+function easyMode() {
+  startButton.textContent = "START";
   topColor = activatedDiv.style.borderTopColor = "red";
   rightColor = activatedDiv.style.borderRightColor = "blue";
   bottomColor = activatedDiv.style.borderBottomColor = "gold";
@@ -35,13 +46,18 @@ function deactivateAll() {
   msg = new SpeechSynthesisUtterance("EASY MODE");
   //Best Voices: Daniel, Karen, Tessa (british)
   //https://codepen.io/matt-west/full/wGzuJ
+  scoreboard.style.display = "block";
+  startButton.style.display = "none";
   if (muted == 0) {
     window.speechSynthesis.speak(msg);
   }
   hardModeStatus = false;
+  score = 0;
+  scoreboard.textContent = "0" + score;
+  gameinProgress = false;
 }
 
-// Individual Lightup Functions
+// Individual Lightup Functions (by User)
 function displayTop() {
   unclick();
   topColor = activatedDiv.style.borderTopColor = "hotpink";
@@ -80,15 +96,18 @@ function displayLeft() {
 }
 
 function hardModeActivate() {
-  if (hardModeStatus == false) {
-    activatedDiv.setAttribute("id", "animatedDiv");
-    msg = new SpeechSynthesisUtterance("hard mode");
-    hardModeStatus = true;
-    setInterval(unclick, 600);
-    if (muted == 0) {
-      window.speechSynthesis.speak(msg);
-    }
+  activatedDiv.setAttribute("id", "animatedDiv");
+  scoreboard.textContent = "0" + score;
+
+  scoreboard.style.display = "block";
+  startButton.style.display = "none";
+  msg = new SpeechSynthesisUtterance("hard mode");
+  hardModeStatus = true;
+  setInterval(unclick, 600);
+  if (muted == 0) {
+    window.speechSynthesis.speak(msg);
   }
+  gameinProgress = false;
 }
 
 [
@@ -160,7 +179,7 @@ function hardModeActivate() {
 });
 
 //easy mode button
-restartButton.addEventListener("click", deactivateAll);
+restartButton.addEventListener("click", easyMode);
 
 //hard mode button
 [hardMode].forEach(function(element) {
@@ -191,3 +210,122 @@ let unmuteButton = document.querySelector(".unmuteButton");
 //mute button
 muteButton.addEventListener("click", mute);
 unmuteButton.addEventListener("click", unmute);
+
+//scoreboard functions
+function increaseScore() {
+  if (computerValues == playerValues) {
+    score += 1;
+    if (score < 10) {
+      scoreboard.textContent = "0" + score;
+      scoreboard.style.marginLeft = "-48px";
+    } else if (score < 20) {
+      scoreboard.textContent = score;
+      scoreboard.style.marginLeft = "-36px";
+    } else if (score < 100) {
+      scoreboard.textContent = score;
+      scoreboard.style.marginLeft = "-48px";
+    } else if (score >= 100) {
+      scoreboard.textContent = score;
+      scoreboard.style.marginLeft = "-60px";
+    }
+  }
+}
+
+// random color function
+
+function calculate(x) {
+  randomNumber = Math.floor(Math.random(100) * 4);
+  console.log(randomNumber);
+  if (randomNumber == 0) {
+    computerValues[x] = "red";
+  } else if (randomNumber == 1) {
+    computerValues[x] = "blue";
+  } else if (randomNumber == 2) {
+    computerValues[x] = "yellow";
+  } else if (randomNumber == 3) {
+    computerValues[x] = "green";
+  }
+}
+
+function computerTurn(score) {
+  calculate(score);
+  if (computerValues[score] == "red") {
+    computerTop();
+  } else if (computerValues[score] == "blue") {
+    computerRight();
+  } else if (computerValues[score] == "yellow") {
+    computerBottom();
+  } else if (computerValues[score] == "green") {
+    computerLeft();
+  }
+}
+
+//test random color
+
+// functions for computer to display its colors
+
+function computerTop() {
+  unclick();
+  topColor = activatedDiv.style.borderTopColor = "hotpink";
+  msg = new SpeechSynthesisUtterance("red");
+  setInterval(unclick, 2000);
+  if (muted == 0) {
+    window.speechSynthesis.speak(msg);
+  }
+}
+
+function computerRight() {
+  unclick();
+  rightColor = activatedDiv.style.borderRightColor = "lightblue";
+  msg = new SpeechSynthesisUtterance("blue");
+  setInterval(unclick, 2000);
+  if (muted == 0) {
+    window.speechSynthesis.speak(msg);
+  }
+}
+
+function computerBottom() {
+  unclick();
+  bottomColor = activatedDiv.style.borderBottomColor = "yellow";
+  msg = new SpeechSynthesisUtterance("yellow");
+  setInterval(unclick, 2000);
+  if (muted == 0) {
+    window.speechSynthesis.speak(msg);
+  }
+}
+
+function computerLeft() {
+  unclick();
+  leftColor = activatedDiv.style.borderLeftColor = "lightgreen";
+  msg = new SpeechSynthesisUtterance("green");
+  setInterval(unclick, 2000);
+  if (muted == 0) {
+    window.speechSynthesis.speak(msg);
+  }
+}
+
+increaseScore();
+
+function startGame() {
+  if (gameinProgress == false) {
+    startButton.style.display = "none";
+
+    msg = new SpeechSynthesisUtterance("3 . 2 . 1");
+    setInterval(unclick, 600);
+    scoreboard.style.display = "block";
+    if (muted == 0) {
+      window.speechSynthesis.speak(msg);
+    }
+    gameinProgress = true;
+  }
+}
+
+//lose game logic here
+//
+//gameinProgress = false ;
+
+[gridbox[27], gridbox[28], gridbox[35], gridbox[36], startButton].forEach(
+  function(element) {
+    element.addEventListener("click", startGame);
+  }
+);
